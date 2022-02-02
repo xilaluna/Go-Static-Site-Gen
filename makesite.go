0da50198f1081e3b6a78016fe6a1fb18bca82447
xiluna <xilaluna2@gmail.com>
2022-02-02 13:17:35 -0800

    [upd] read in file

--- makesite.go
+++ makesite.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func readFile() {
+	fileContents, err := ioutil.ReadFile("first-post.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(string(fileContents))
+}
 
 func main() {
+	readFile()
 	fmt.Println("Hello, world!")
 }
