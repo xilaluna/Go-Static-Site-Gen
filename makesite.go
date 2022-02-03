99085dc6feacf301dfeb1c9ce86a6623a05af3f8
xiluna <xilaluna2@gmail.com>
2022-02-02 18:21:06 -0800

    [upd] make scruct for new Page and edit temp

--- makesite.go
+++ makesite.go
@@ -5,6 +5,15 @@ import (
 	"io/ioutil"
 )
 
+type paragraph struct {
+	Data string
+}
+
+type Page struct {
+	Heading string
+	Content []paragraph
+}
+
 func readFile() {
 	fileContents, err := ioutil.ReadFile("first-post.txt")
 	if err != nil {
